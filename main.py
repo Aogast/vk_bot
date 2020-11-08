b4c35126eec184223ffe012956390523de70427b
HTSK-company <syrov.art@gmail.com>
2020-11-08 16:51:39 +0300

    all 1.0

--- main.py
+++ main.py
@@ -19,16 +19,45 @@ for event in longpoll.listen():
             if event.from_chat:
                 msg = event.text.lower()
                 id = event.chat_id
-                if msg in ['никита']:
-                    sender(id, 'вы хороший человек')
+                if msg == 'help':
+                    sender(id, '/add + время - назначить встречу\n'
+                               '/del + время - удалить встречу\n'
+                               '/list - показать список встерчь\n'
+                               'формат времени :\n'
+                               '13:47 понедельник')
+                if msg in '/привет':
+                    sender(id, 'хорошего дня')
                 if '/add' in msg:
                     f = open("data.txt", 'r')
                     notifications = f.read().split('\n')
                     f.close()
-                    f = open('data.txt', 'w')
-                    notifications.append(msg[5:])
-                    f.write('\n'.join(notifications))
+                    if msg[5:] not in notifications:
+                        f = open('data.txt', 'w')
+                        notifications.append(msg[5:])
+                        f.write('\n'.join(notifications))
+                        f.close()
+                        sender(id, 'Встреча назначена успешно.')
+                    else:
+                        sender(id, 'На данное время встреча уже назначена.')
+                if '/del' in msg:
+                    f = open("data.txt", 'r')
+                    notifications = f.read().split('\n')
                     f.close()
-                    print(notifications)
+                    if msg[5:] in notifications:
+                        f = open('data.txt', 'w')
+                        notifications.pop(notifications.index(msg[5:]))
+                        f.write('\n'.join(notifications))
+                        sender(id, 'Встреча успешно отменена.')
+                        f.close()
+                    else:
+                        sender(id, 'На данное время не былозапланированных встреч.')
+                if '/list' in msg:
+                    f = open("data.txt", 'r')
+                    notifications = f.read().split('\n')
+                    if notifications != ['']:
+                        sender(id, '\n'.join(notifications))
+                    else:
+                        sender(id, 'Пока что назначенных встреч нет. Спи скокойно, не беспокоясь о дедлайнах')
+
 
 
--- notification.py
+++ notification.py
@@ -12,19 +12,40 @@ longpoll = VkLongPoll(vk_session)
 def sender(id, text):
     vk_session.method('messages.send', {'chat_id': id, 'message': text, 'random_id': 0})
 
+
 while True:
-    sleep(5)
+    sleep(35)
     current_datetime = datetime.now()
-    minut = str(current_datetime.minute)
+    minut = current_datetime.minute
+    hour = current_datetime.hour
+    if minut > 49:
+        hour += 1
+        minut = 60 - minut
+    else:
+        minut += 10
+    minut = str(minut)
     minut = '0' * (2 - len(minut)) + minut
-    hour = str(current_datetime.hour)
+    hour = str(hour)
     hour = '0' * (2 - len(hour)) + hour
     time = hour + ':' + minut + ' ' + days[datetime.weekday(current_datetime)]
+    if int(minut) > 9:
+        minut = str((50 + int(minut)) % 60)
+        hour = str(int(hour) + 1)
+    else:
+        minut = int(minut) + 50
+    minut = '0' * (2 - len(minut)) + minut
+    hour = '0' * (2 - len(hour)) + hour
+    time2 = hour + ':' + minut + ' ' + days[datetime.weekday(current_datetime)]
     f = open("data.txt", 'r')
     notifications = f.read().split('\n')
     print(notifications)
     if time in notifications:
-        sender(1, 'Собранре через 10 минут. Пожалуйста, не опаздывайте')
-        notifications.pop(notifications.index(time))
+        sender(1, 'Собранре через 10 минут.'
+                  ' Пожалуйста, не опаздывайте')
+    if time2 in notifications:
+        sender(1, 'Собранре через час. '
+                  'Прочитай материал курса,'
+                  ' если ты этого еще не сделал.')
+    print(time, time2)
     f.close()
 
